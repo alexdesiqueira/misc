00e67899d4982944766a1a4b85c5fe95b95fb69a
alexdesiqueira <alex.desiqueira@igdore.org>
2020-03-23 06:40:16 -0700

    Adding skimage_triaging + week 12

---
+++ skimage_triaging/2020/wk12_2020/auto/skimage_triage_2020-wk12.el
@@ -0,0 +1,42 @@
+(TeX-add-style-hook
+ "skimage_triage_2020-wk12"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orga8056cc"
+    "sec:org89c156a"
+    "sec:org6d4d19b"
+    "sec:orgd57ca1e"
+    "sec:orgcfb9810"
+    "sec:org6905c17"
+    "sec:org68efdd9"))
+ :latex)
+
